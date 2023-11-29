7568ae188da98849d2ecc24d473c3fdf02fa7f49
Johan Larsson <johanlarsson@outlook.com>
2023-11-29 16:04:25 +0100

    build: set version through version.py file

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
+import os
 from glob import glob
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "0.1.0"
+# Setup boilerplate below this line.
 
-# The main interface is through Pybind11Extension.
-# * You can add cxx_std=11/14/17, and then build_ext can be removed.
-# * You can set include_pybind11=false to add the include directory yourself,
-#   say from a submodule.
-#
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+# Set version from version.py
+package_root = os.path.abspath(os.path.dirname(__file__))
+version = {}
+with open(os.path.join(package_root, "sortedl1/version.py")) as fp:
+    exec(fp.read(), version)
+__version__ = version["__version__"]
 
 ext_modules = [
     Pybind11Extension(
@@ -29,11 +28,9 @@ setup(
     author="Johan Larsson",
     author_email="johanlarsson@outlook.com",
     url="https://github.com/jolars/sortedl1",
-    description="Sorted L1 Penalized Estimation",
+    description="Sorted L-One Penalized Estimation (SLOPE)",
     long_description="",
     ext_modules=ext_modules,
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
 )
