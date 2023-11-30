9675a7dcd50e7c128eb83454f4df8c00472930de
Johan Larsson <johanlarsson@outlook.com>
2023-11-30 11:50:28 +0100

    build: move most setup.py options into setup.cfg

--- setup.py
+++ setup.py
@@ -1,45 +1,17 @@
-import os
 from glob import glob
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-# Setup boilerplate below this line.
-
-# Set version from version.py
-package_root = os.path.abspath(os.path.dirname(__file__))
-version = {}
-with open(os.path.join(package_root, "sortedl1/version.py")) as fp:
-    exec(fp.read(), version)
-__version__ = version["__version__"]
-
-install_requires = ["numpy", "scipy", "scikit-learn"]
-
 ext_modules = [
     Pybind11Extension(
         "_sortedl1",
         sorted(glob("src/**/*.cpp", recursive=True)),
-        define_macros=[("VERSION_INFO", __version__)],
         include_dirs=["external/"],
     ),
 ]
 
 setup(
-    name="sortedl1",
-    version=__version__,
-    author="Johan Larsson",
-    author_email="johanlarsson@outlook.com",
-    url="https://github.com/jolars/sortedl1",
-    description="Sorted L-One Penalized Estimation (SLOPE)",
-    long_description="",
-    install_requires=install_requires,
-    extras_require={
-        "docs": [
-            "sphinx",
-            "sphinx_rtd_theme",
-        ]
-    },
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
-    zip_safe=False,
 )
