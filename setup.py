9962c6fdfc3332205729b323d4b507ca4805120a
Johan Larsson <johanlarsson@outlook.com>
2023-11-30 11:56:26 +0100

    build: explicitly ask for c++14 standard

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from glob import glob
 
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11.setup_helpers import Pybind11Extension
 from setuptools import setup
 
-ext_modules = [
-    Pybind11Extension(
-        "_sortedl1",
-        sorted(glob("src/**/*.cpp", recursive=True)),
-        include_dirs=["external/"],
-    ),
-]
-
 setup(
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
+    ext_modules=[
+        Pybind11Extension(
+            "_sortedl1",
+            sorted(glob("src/**/*.cpp", recursive=True)),
+            include_dirs=["external/"],
+            cxx_std=14,
+        )
+    ]
 )
